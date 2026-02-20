a35c5814b53caaf18a71159495c9025bf7f4d4fc
Kason Stubbart <kgstubbart@gmail.com>
2026-02-19 18:22:03 -0700

    password and email change test

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "playwright-test-coverage";
 
 test("updateUser", async ({ page }) => {
-	const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+	let email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
 	await page.goto("/");
 	await page.getByRole("link", { name: "Register" }).click();
 	await page.getByRole("textbox", { name: "Full name" }).fill("pizza diner");
@@ -24,6 +24,10 @@ test("updateUser", async ({ page }) => {
     await page.getByRole('button', { name: 'Edit' }).click();
     await expect(page.locator('h3')).toContainText('Edit user');
     await page.getByRole('textbox').first().fill('pizza dinerx');
+
+    email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+    await page.locator('input[type="email"]').fill(email);
+    await page.locator('#password').fill('dinerx');
     await page.getByRole('button', { name: 'Update' }).click();
 
     await page.waitForSelector('[role="dialog"].hidden', { state: 'attached' });
@@ -34,10 +38,11 @@ test("updateUser", async ({ page }) => {
     await page.getByRole('link', { name: 'Login' }).click();
 
     await page.getByRole('textbox', { name: 'Email address' }).fill(email);
-    await page.getByRole('textbox', { name: 'Password' }).fill('diner');
+    await page.getByRole('textbox', { name: 'Password' }).fill('dinerx');
     await page.getByRole('button', { name: 'Login' }).click();
 
     await page.getByRole('link', { name: 'pd' }).click();
 
     await expect(page.getByRole('main')).toContainText('pizza dinerx');
+    await expect(page.getByRole('main')).toContainText(email);
 });
