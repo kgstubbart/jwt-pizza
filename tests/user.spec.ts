dfd325f0bd5b907cf3f6e9b85ee22584899058a3
Kason Stubbart <kgstubbart@gmail.com>
2026-02-19 23:20:26 -0700

    playwright tests for admin dashboard list of users and deleting a user

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -46,3 +46,41 @@ test("updateUser", async ({ page }) => {
     await expect(page.getByRole('main')).toContainText('pizza dinerx');
     await expect(page.getByRole('main')).toContainText(email);
 });
+
+test("listUsers", async ({ page }) => {
+    await page.goto("/");
+    await page.getByRole("link", { name: "Login" }).click();
+    await page.getByRole("textbox", { name: "Email address" }).fill("a@jwt.com");
+    await page.getByRole("textbox", { name: "Password" }).fill("admin");
+    await page.getByRole("button", { name: "Login" }).click();
+    await page.getByRole("link", { name: "Admin" }).click();
+    
+    await expect(page.getByRole('main')).toContainText('Users');
+    await expect(page.getByRole('main')).toContainText('Name');
+    await expect(page.getByRole('main')).toContainText('Email');
+    await expect(page.getByRole('main')).toContainText('Role');
+    await page.getByRole('textbox', { name: 'Name' }).click();
+    await page.getByRole('textbox', { name: 'Name' }).fill('kai');
+    await page.getByRole('button', { name: 'Search' }).click();
+    await expect(page.getByRole('main')).toContainText('Kai Chen');
+});
+
+test("deleteUser", async ({ page }) => {
+    let user = `user${Math.floor(Math.random() * 10000)}`;
+    let email = `${user}@jwt.com`;
+	await page.goto("/");
+	await page.getByRole("link", { name: "Register" }).click();
+	await page.getByRole("textbox", { name: "Full name" }).fill(user);
+	await page.getByRole("textbox", { name: "Email address" }).fill(email);
+	await page.getByRole("textbox", { name: "Password" }).fill("diner");
+	await page.getByRole("button", { name: "Register" }).click();
+    await page.getByRole('link', { name: 'Logout' }).click();
+
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole("textbox", { name: "Email address" }).fill("a@jwt.com");
+    await page.getByRole("textbox", { name: "Password" }).fill("admin");
+    await page.getByRole("button", { name: "Login" }).click();
+    await page.getByRole("link", { name: "Admin" }).click();
+
+    await page.getByRole('button', { name: `Delete ${user}` }).click();
+});
